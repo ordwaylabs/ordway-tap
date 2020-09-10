7a39172ac24330dea6ca5b4b6cf9b97e854a23cf
Mubarak K K <mubarak.kollaru-kandiyil@ordwaylabs.com>
2020-09-10 18:36:31 -0400

    ODP-4311 - Initialized python tap application

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="ordway-tap",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["ordway_tap"],
+    install_requires=[
+        "singer-python>=5.0.12",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    ordway-tap=ordway_tap:main
+    """,
+    packages=["ordway_tap"],
+    package_data = {
+        "schemas": ["ordway_tap/schemas/*.json"]
+    },
+    include_package_data=True,
+)
