f800fce867ddecdcbc61f2de0ee49602ddf736b0
nicholas-ramsey <me@nicholas-ramsey.com>
2020-10-28 09:44:36 -0500

    Added .dev and .testing extra_requires, use find_packages; and added keywords, long_description, and more classifiers for PyPi

--- setup.py
+++ setup.py
@@ -1,27 +1,54 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import join, abspath, dirname
+from re import search as re_search
+
+PACKAGE_NAME = "ordway-tap"
+PACKAGE_DIR = PACKAGE_NAME.replace("-", "_")
+ROOT_DIR = abspath(dirname(__file__))
+
+INSTALL_REQUIRES = [
+    "singer-python>=5.0.12",
+    "requests",
+    "kafka-python",
+    "inflection",
+]
+
+EXTRA_REQUIRES = {
+    "dev": ["black==20.8b1", "pylint==2.6.0", "tox==3.20.1"],
+    "testing": ["mypy==0.790", "pytest==6.1.1", "pytest-cov==2.10.1"],
+}
+
+with open(join(ROOT_DIR, "README.md"), encoding="utf-8") as readme_file:
+    README = readme_file.read()
+
+with open(
+    join(ROOT_DIR, PACKAGE_DIR, "__version__.py"), encoding="utf-8"
+) as version_file:
+    VERSION = re_search(r'__version__\s+?=\s+?"(.+)"', version_file.read()).group(1)
 
 setup(
     name="ordway-tap",
-    version="0.1.0",
+    version=VERSION,
     description="Singer.io tap for extracting data",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Stitch",
     url="http://singer.io",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["ordway_tap"],
-    install_requires=[
-        "singer-python>=5.0.12",
-        "requests",
-        "kafka-python",
-        "inflection"
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
     ],
+    py_modules=["ordway_tap"],
+    install_requires=INSTALL_REQUIRES,
     entry_points="""
     [console_scripts]
     ordway-tap=ordway_tap:main
     """,
-    packages=["ordway_tap"],
-    package_data = {
-        "schemas": ["ordway_tap/schemas/*.json"]
-    },
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"schemas": ["ordway_tap/schemas/*.json"]},
     include_package_data=True,
+    extras_require=EXTRA_REQUIRES,
+    keywords="singer ordway tap",
 )
