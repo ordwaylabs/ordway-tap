415f1165bf7daf1c160576393431bf95363df02d
nicholas-ramsey <me@nicholas-ramsey.com>
2020-11-06 10:17:03 -0600

    key_properties are now specified under stream definition classes; added get_key_properties, get_replication_method, and get_replication_key helpers

--- ordway_tap/property.py
+++ ordway_tap/property.py
@@ -1,37 +1,45 @@
-from singer.metadata import get_standard_metadata, to_map as mdata_to_map, write as mdata_write, to_list as mdata_to_list
-
-KEY_PROPERTIES_MAP = {
-    'customers': ['company_id', 'customer_id'],
-    'subscriptions': ['company_id', 'subscription_id', 'subscription_line_id'],
-    'invoices': ['company_id', 'invoice_id', 'invoice_line_no'],
-    'payments': ['company_id', 'payment_id'],
-    'credits': ['company_id', 'credit_id'],
-    'refunds': ['company_id', 'refund_id'],
-    'billing_schedules': ['company_id', 'billing_schedule_id'],
-    'revenue_schedules': ['company_id', 'revenue_schedule_id'],
-    'products': ['company_id', 'product_id'],
-    'orders': ['company_id', 'order_id', 'order_line_no']
-}
+from typing import Optional, List
+from singer.metadata import (
+    get_standard_metadata,
+    to_map as mdata_to_map,
+    write as mdata_write,
+    to_list as mdata_to_list,
+)
+from .streams import AVAILABLE_STREAMS
 
 
-def get_key_properties(stream):
-    key_properties = KEY_PROPERTIES_MAP[stream]
+def get_key_properties(tap_stream_id: str) -> Optional[List[str]]:
+    """ Retrieves a stream's key_properties """
+
+    key_properties = AVAILABLE_STREAMS[tap_stream_id].key_properties
+
     if not key_properties:
         return None
+
     return key_properties
 
 
+def get_replication_method(tap_stream_id: str) -> Optional[str]:
+    return getattr(AVAILABLE_STREAMS[tap_stream_id], "replication_method", None)
+
+
+def get_replication_key(tap_stream_id: str) -> Optional[str]:
+    return getattr(AVAILABLE_STREAMS[tap_stream_id], "replication_key", None)
+
+
 def get_stream_metadata(tap_stream_id, schema_dict):
+    stream_def = AVAILABLE_STREAMS[tap_stream_id]
+
     metadata = get_standard_metadata(
         schema=schema_dict,
-        key_properties=KEY_PROPERTIES_MAP.get(tap_stream_id),
-        valid_replication_keys=["updated_date"],
+        key_properties=get_key_properties(tap_stream_id),
+        valid_replication_keys=stream_def.valid_replication_keys,
     )
 
     metadata = mdata_to_map(metadata)
 
-    for field_name in schema_dict['properties'].keys():
-        # selected-by-default doesn't currently work as intended. 
+    for field_name in schema_dict["properties"].keys():
+        # selected-by-default doesn't currently work as intended.
         # A PR has been made for a fix, but it hasn't been merged yet:
         # https://github.com/singer-io/singer-python/pull/121
         # Writing regardless for when PR is merged.
@@ -42,4 +50,3 @@ def get_stream_metadata(tap_stream_id, schema_dict):
     metadata = mdata_write(metadata, (), "selected", True)
 
     return mdata_to_list(metadata)
-    
