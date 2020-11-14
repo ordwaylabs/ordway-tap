d024f2c1799f4157faadf38c29b4107e9207fed3
nicholas-ramsey <me@nicholas-ramsey.com>
2020-11-14 09:29:34 -0600

    Removed CreditTransformer, StatementTransformer, and PaymentTransformer

--- ordway_tap/transformers/__init__.py
+++ ordway_tap/transformers/__init__.py
@@ -1,11 +1,8 @@
 from .base import RecordTransformer
 from .transformers import (
     InvoiceTransformer,
-    PaymentTransformer,
-    StatementTransformer,
     SubscriptionTransformer,
     OrderTransformer,
     CustomerTransformer,
-    CreditTransformer,
     BillingScheduleTransformer,
 )
