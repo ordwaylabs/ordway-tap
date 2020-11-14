7dbcaa15f6974e0a9b70e0255870422944e32af7
nicholas-ramsey <me@nicholas-ramsey.com>
2020-11-14 09:27:07 -0600

    Added filter hook / general refactoring

--- ordway_tap/streams/base.py
+++ ordway_tap/streams/base.py
@@ -3,15 +3,15 @@ from typing import (
     Dict,
     Any,
     List,
-    Optional,
     Sequence,
     Union,
     Type,
     Tuple,
     Generator,
+    Optional,
+    Callable,
 )
 from abc import ABC, abstractmethod
-from collections.abc import Iterable as IterableCollection
 from singer import get_logger
 from singer.metadata import to_map as mdata_to_map
 from ..base import DataContext
@@ -21,11 +21,14 @@ from ..utils import (
 
 if TYPE_CHECKING:
     from datetime import datetime
-    from singer.catalog import CatalogEntry
+    from singer.catalog import Catalog, CatalogEntry
     from ..transformers import RecordTransformer
     from ..api import RequestHandler
 
 LOGGER = get_logger()
+
+# pylint: disable=invalid-name
+_FILTER_HOOK = Callable[[Dict[str, str], DataContext], bool]
 
 
 def _attach_tap_stream_id(
@@ -37,14 +40,20 @@ def _attach_tap_stream_id(
         yield (tap_stream_id, record)
 
 
+# pylint: disable=too-many-instance-attributes
 class StreamABC(ABC):
     """ Stream abstract base class """
 
     replication_method = "INCREMENTAL"
-    replication_key = "updated_date"
-    valid_replication_keys = ["updated_date"]
+    replication_key: Optional[str] = "updated_date"
+    valid_replication_keys: Sequence[str] = ["updated_date"]
 
-    def __init__(self, catalog: "Catalog", config: Dict[str, Any]):
+    def __init__(
+        self,
+        catalog: "Catalog",
+        config: Dict[str, Any],
+        filter_hook: Optional[_FILTER_HOOK] = None,
+    ):
         self.catalog_entry = catalog.get_stream(self.tap_stream_id)
         self.config = config
         self.replication_key = self.catalog_entry.replication_key
@@ -52,8 +61,13 @@ class StreamABC(ABC):
 
         self.mapped_metadata = mdata_to_map(self.catalog_entry.metadata)
         self.schema_dict = self.catalog_entry.schema.to_dict()
+        self.filter_hook: _FILTER_HOOK = (
+            (lambda *_, **__: False) if filter_hook is None else filter_hook
+        )
 
         self._check_replication_config()
+
+        self._is_selected: Optional[bool] = None
 
     @property
     def is_valid_incremental(self) -> bool:
@@ -68,16 +82,22 @@ class StreamABC(ABC):
     def is_selected(self) -> bool:
         """ Whether the stream is selected """
 
-        return self.catalog_entry.is_selected()
+        if self._is_selected is None:
+            selected = self.catalog_entry.is_selected()
+            self._is_selected = selected
+
+            return selected
+
+        return self._is_selected
 
     @property
     @abstractmethod
-    def transformer(self) -> "RecordTransformer":
-        pass  # pragma: no cover
+    def transformer_class(self) -> Type["RecordTransformer"]:
+        """The transformer class to apply to all RECORDs"""
 
     @property
     @abstractmethod
-    def tap_stream_id(self) -> Optional[str]:
+    def tap_stream_id(self) -> str:
         pass  # pragma: no cover
 
     @property
@@ -85,17 +105,16 @@ class StreamABC(ABC):
     def key_properties(self) -> Sequence[str]:
         pass  # pragma: no cover
 
-    def should_output(self, record: Dict[str, Any]) -> bool:
-        return True
-
     def _check_replication_config(self) -> None:
         if self.replication_key is None:
-            return None
+            return None  # type: ignore[unreachable]
 
         if self.replication_key not in self.valid_replication_keys:
             raise ValueError(
                 f'"{self.replication_key}" is not a valid replication key for {self.tap_stream_id}'
             )
+
+        return None
 
 
 class Substream(StreamABC):
@@ -108,7 +127,7 @@ class ResponseSubstream(Substream):
     @property
     @abstractmethod
     def path(self) -> Tuple[str, ...]:
-        pass  # pragma: no cover
+        """A tuple of dictionary keys from which the substream records can be found"""
 
 
 class EndpointSubstream(Substream):
@@ -119,8 +138,10 @@ class EndpointSubstream(Substream):
     def request_handler(self) -> "RequestHandler":
         pass
 
-    def sync(self, parent_record: Dict[str, Any], filter_datetime: "datetime") -> None:
-        with self.transformer() as transformer:
+    def sync(
+        self, parent_record: Dict[str, Any], filter_datetime: "datetime"
+    ) -> Generator[Tuple[str, Dict[str, Any]], None, None]:
+        with self.transformer_class() as transformer:
             context = DataContext(
                 stream=self,
                 filter_datetime=filter_datetime,
@@ -129,6 +150,9 @@ class EndpointSubstream(Substream):
             )
 
             for record in self.request_handler.fetch(context=context):
+                if self.filter_hook(record, context):
+                    continue
+
                 yield from _attach_tap_stream_id(
                     self.tap_stream_id,
                     transformer.transform(
@@ -154,14 +178,21 @@ class Stream(StreamABC):
 
         return len(self.substreams) > 0
 
-    def instantiate_substreams(self, catalog: "Catalog") -> None:
-        for i in range(len(self.substreams)):
-            self.substreams[i] = self.substreams[i](catalog, self.config)
+    def instantiate_substreams(
+        self,
+        catalog: "Catalog",
+        filter_hook: Optional[Callable[[Dict[str, str], DataContext], bool]] = None,
+    ) -> None:
+        for i, substream_class in enumerate(self.substreams):
+            if isinstance(substream_class, Substream):
+                continue
+
+            self.substreams[i] = substream_class(catalog, self.config, filter_hook)
 
     def sync(
         self, filter_datetime: "datetime"
     ) -> Generator[Tuple[str, Dict[str, Any]], None, None]:
-        with self.transformer() as transformer:
+        with self.transformer_class() as transformer:
             context = DataContext(
                 stream=self,
                 filter_datetime=filter_datetime,
@@ -170,6 +201,12 @@ class Stream(StreamABC):
 
             for record in self.request_handler.fetch(context=context):
                 yield from self.sync_substreams(record, filter_datetime)
+
+                # Skip primary stream if record is filtered,
+                # but give substreams a chance to perform
+                # their own filtering.
+                if self.filter_hook(record, context):
+                    continue
 
                 yield from _attach_tap_stream_id(
                     self.tap_stream_id,
@@ -181,11 +218,43 @@ class Stream(StreamABC):
                     ),
                 )
 
+    def sync_sub_records(
+        self,
+        substream: ResponseSubstream,
+        parent_record: Dict[str, Any],
+        filter_datetime: "datetime",
+    ) -> Generator[Tuple[str, Dict[str, Any]], None, None]:
+        """Syncs an ResponseSubstream records given the `parent_record`"""
+
+        context = DataContext(
+            stream=substream,
+            filter_datetime=filter_datetime,
+            parent_record=parent_record,
+            tap_stream_id=substream.tap_stream_id,
+        )
+
+        denested_value = denest(parent_record, substream.path)
+
+        with substream.transformer_class() as transformer:
+            for sub_record in denested_value:
+                if self.filter_hook(sub_record, context):
+                    continue
+
+                yield from _attach_tap_stream_id(
+                    substream.tap_stream_id,
+                    transformer.transform(
+                        sub_record,
+                        substream.schema_dict,
+                        context=context,
+                        metadata=substream.mapped_metadata,
+                    ),
+                )
+
     def sync_substreams(
         self, parent_record: Dict[str, Any], filter_datetime: "datetime"
     ) -> Generator[Tuple[str, Dict[str, Any]], None, None]:
         for substream in self.substreams:
-            if not isinstance(substream, (ResponseSubstream, EndpointSubstream)):
+            if not isinstance(substream, Substream):
                 raise TypeError(
                     f"Substream {substream.tap_stream_id} should be initialized before calling."
                 )
@@ -194,26 +263,8 @@ class Stream(StreamABC):
                 continue
 
             if isinstance(substream, ResponseSubstream):
-                denested_value = denest(parent_record, substream.path)
-
-                if not isinstance(denested_value, IterableCollection):
-                    denested_value = [denested_value]
-
-                with substream.transformer() as transformer:
-                    for sub_record in denested_value:
-                        yield from _attach_tap_stream_id(
-                            substream.tap_stream_id,
-                            transformer.transform(
-                                sub_record,
-                                substream.schema_dict,
-                                context=DataContext(
-                                    stream=substream,
-                                    filter_datetime=filter_datetime,
-                                    parent_record=parent_record,
-                                    tap_stream_id=substream.tap_stream_id,
-                                ),
-                                metadata=substream.mapped_metadata,
-                            ),
-                        )
+                yield from self.sync_sub_records(
+                    substream, parent_record, filter_datetime
+                )
             elif isinstance(substream, EndpointSubstream):
                 yield from substream.sync(parent_record, filter_datetime)
