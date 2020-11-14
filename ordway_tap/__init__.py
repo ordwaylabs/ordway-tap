12b4b86ca0281aaf6d18f557fce5eb01afc0a07f
nicholas-ramsey <me@nicholas-ramsey.com>
2020-11-14 09:40:01 -0600

    Refactoring / added filter_record hook

--- ordway_tap/__init__.py
+++ ordway_tap/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import os
 import json
-import singer
-from typing import TYPE_CHECKING, Dict, Any, List
+from typing import TYPE_CHECKING, Dict, Any, List, Union, Optional, Tuple
 from _datetime import datetime
-from singer import utils, metadata
+from singer import utils, metadata, get_logger
 from singer.utils import (
     handle_top_exception,
     parse_args,
@@ -12,11 +11,12 @@ from singer.utils import (
     strptime_to_utc,
     strftime,
 )
-from singer.messages import write_state, write_message, RecordMessage
+from singer.messages import write_state, write_schema, write_message, RecordMessage
 from singer.catalog import Catalog, CatalogEntry
 from singer.schema import Schema
-from singer.bookmarks import get_bookmark, write_bookmark, set_currently_syncing
-import ordway_tap.kafka_consumer
+from singer.bookmarks import write_bookmark, set_currently_syncing
+
+# import ordway_tap.kafka_consumer
 import ordway_tap.configs as TAP_CONFIG
 from .property import (
     get_key_properties,
@@ -30,11 +30,13 @@ from .utils import (
     get_version,
     get_full_table_version,
     write_activate_version,
+    get_filter_datetime,
 )
 from .api.consts import DEFAULT_API_VERSION
 
 if TYPE_CHECKING:
-    from .streams.base import StreamABC
+    from .streams.base import Stream, Substream
+    from .base import DataContext
 
 
 REQUIRED_CONFIG_KEYS = ["api_credentials", "start_date"]
@@ -44,18 +46,18 @@ REQUIRED_API_CREDENTIAL_KEYS = [
     "x_user_email",
     "x_user_token",
 ]
-LOGGER = singer.get_logger()
+LOGGER = get_logger()
 
 
-def get_abs_path(path: str) -> str:
+def _get_abs_path(path: str) -> str:
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
 def load_schemas() -> Dict[str, Any]:
     """ Load schemas from schemas folder """
     schemas = {}
-    for filename in os.listdir(get_abs_path("schemas")):
-        path = get_abs_path("schemas") + "/" + filename
+    for filename in os.listdir(_get_abs_path("schemas")):
+        path = _get_abs_path("schemas") + "/" + filename
         file_raw = filename.replace(".json", "")
         with open(path) as file:
             schemas[file_raw] = Schema.from_dict(json.load(file))
@@ -88,12 +90,151 @@ def discover() -> Catalog:
     return Catalog(streams)
 
 
+def filter_record(record: Dict[str, Any], context: "DataContext") -> bool:
+    """Filter hook for ensuring records are less than filter_datetime for
+    streams that don't support filtering by updated_date via Ordway's API
+    """
+
+    record_updated_date = record.get("updated_date")
+
+    if record_updated_date is None:
+        LOGGER.debug(
+            "Skipping record for stream '%s': updated_date is None or non-existent",
+            context.tap_stream_id,
+        )
+
+        return False
+
+    if strptime_to_utc(record_updated_date) <= context.filter_datetime:
+        LOGGER.debug(
+            "Skipping record for stream '%s': %s is <= %s",
+            context.tap_stream_id,
+            record_updated_date,
+            context.filter_datetime,
+        )
+
+        return True
+
+    return False
+
+
+def handle_record(
+    tap_stream_id: str,
+    record: Dict[str, Any],
+    stream_def: Union["Stream", "Substream"],
+    stream_version: Optional[int],
+    state: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Handles a single record's emission"""
+
+    print_record(tap_stream_id, record, version=stream_version)
+
+    if not is_substream(stream_def):
+        state = set_currently_syncing(state, tap_stream_id)
+
+    if not stream_def.is_valid_incremental:
+        return state
+
+    replication_key = stream_def.replication_key
+
+    # mypy ignoring is_valid_incremental above
+    bookmark_date = record.get(replication_key)  # type: ignore
+
+    if bookmark_date is None:
+        LOGGER.warning(
+            'State not updated. Replication key "%s" not found in record for stream "%s": %s',
+            replication_key,
+            tap_stream_id,
+            record,
+        )
+
+        return state
+
+    LOGGER.info("Adding bookmark for %s at %s", tap_stream_id, bookmark_date)
+
+    state = write_bookmark(
+        state,
+        tap_stream_id,
+        replication_key,
+        bookmark_date,
+    )
+
+    write_state(state)
+
+    return state
+
+
+_STREAM_DEFS = Dict[str, Union["Stream", "Substream"]]  # pylint: disable=invalid-name
+_STREAM_VERSIONS = Dict[str, Optional[int]]  # pylint: disable=invalid-name
+
+# Could be refactored
+# pylint: disable=too-many-arguments
+def prepare_stream(
+    tap_stream_id: str,
+    stream_defs: _STREAM_DEFS,
+    stream_versions: _STREAM_VERSIONS,
+    catalog: Catalog,
+    config: Dict[str, Any],
+    state: Dict[str, Any],
+) -> datetime:
+    """Prepares a stream and any of its substreams by instantiating them and
+    handling their preliminary Singer messages
+    """
+
+    # mypy isn't properly considering is_substream
+    stream_def: "Stream" = AVAILABLE_STREAMS[tap_stream_id](catalog, config, filter_record)  # type: ignore
+    stream_defs[stream_def.tap_stream_id] = stream_def
+
+    if stream_def.has_substreams:
+        stream_def.instantiate_substreams(catalog, filter_record)
+
+        for substream_def in stream_def.substreams:
+            if not substream_def.is_selected:
+                continue
+
+            # ignored type errors below seem to be caused by same issue as
+            # https://github.com/python/mypy/issues/8993
+            stream_defs[substream_def.tap_stream_id] = substream_def  # type: ignore
+            substream_version = get_full_table_version()
+            stream_versions[substream_def.tap_stream_id] = substream_version  # type: ignore
+
+            write_schema(
+                stream_name=substream_def.tap_stream_id,  # type: ignore
+                schema=substream_def.schema_dict,
+                key_properties=substream_def.key_properties,
+            )
+
+            # Substreams and their parent stream are all FULL_TABLE, so
+            # no need to check substream bookmarks.
+            write_activate_version(substream_def.tap_stream_id, substream_version)  # type: ignore
+
+    write_schema(
+        stream_name=stream_def.tap_stream_id,
+        schema=stream_def.schema_dict,
+        key_properties=stream_def.key_properties,
+    )
+
+    filter_datetime = get_filter_datetime(stream_def, config["start_date"], state)
+    stream_version = get_version(stream_def, config["start_date"], filter_datetime)
+    stream_versions[stream_def.tap_stream_id] = stream_version
+
+    if stream_version is not None:
+        write_activate_version(
+            stream_def.tap_stream_id,
+            stream_version,
+        )
+
+    return filter_datetime
+
+
 def sync(config: Dict[str, Any], state: Dict[str, Any], catalog: Catalog) -> None:
     # For looking up Catalog-configured streams more efficiently
     # later Singer stores catalog entries as a list and iterates
     # over it with .get_stream()
-    stream_defs = {}
-    stream_versions = {}
+    stream_defs: Dict[str, Union["Stream", "Substream"]] = {}
+    stream_versions: Dict[str, Optional[int]] = {}
+
+    check_dependency_conflicts(catalog)
 
     for stream in catalog.get_selected_streams(state):
         if is_substream(AVAILABLE_STREAMS[stream.tap_stream_id]):
@@ -106,84 +247,21 @@ def sync(config: Dict[str, Any], state: Dict[str, Any], catalog: Catalog) -> Non
 
         LOGGER.info("Syncing stream: %s", stream.tap_stream_id)
 
-        stream_def = AVAILABLE_STREAMS[stream.tap_stream_id](catalog, config)
-        stream_defs[stream_def.tap_stream_id] = stream_def
-
-        if stream_def.has_substreams:
-            stream_def.instantiate_substreams(catalog)
-
-            for substream_def in stream_def.substreams:
-                stream_defs[substream_def.tap_stream_id] = substream_def
-                substream_version = get_full_table_version()
-                stream_versions[substream_def.tap_stream_id] = substream_version
-
-                singer.write_schema(
-                    stream_name=substream_def.tap_stream_id,
-                    schema=substream_def.schema_dict,
-                    key_properties=substream_def.key_properties,
-                )
-
-                # Substreams and their parent stream are all FULL_TABLE, so
-                # no need to check substream bookmarks.
-                write_activate_version(substream_def.tap_stream_id, substream_version)
-
-        singer.write_schema(
-            stream_name=stream_def.tap_stream_id,
-            schema=stream_def.schema_dict,
-            key_properties=stream_def.key_properties,
+        filter_datetime = prepare_stream(
+            stream.tap_stream_id, stream_defs, stream_versions, catalog, config, state
         )
-
-        filter_datetime = config["start_date"]
-
-        if stream_def.is_valid_incremental:
-            filter_datetime = get_bookmark(
-                state, stream.tap_stream_id, stream_def.replication_key, filter_datetime
-            )
-
-        filter_datetime = strptime_to_utc(filter_datetime)
+        stream_def = stream_defs[stream.tap_stream_id]
 
         LOGGER.info("Querying since: %s", filter_datetime)
 
-        stream_version = get_version(stream_def, config["start_date"], filter_datetime)
-        stream_versions[stream_def.tap_stream_id] = stream_version
-
-        if stream_version is not None:
-            write_activate_version(
-                stream_def.tap_stream_id,
-                stream_version,
-            )
-
-        for tap_stream_id, record in stream_def.sync(filter_datetime):
-            print_record(tap_stream_id, record, version=stream_versions[tap_stream_id])
-
-            if not stream_defs[tap_stream_id].is_valid_incremental:
-                continue
-
-            bookmark_date = record.get(stream_defs[tap_stream_id].replication_key)
-
-            if bookmark_date is None:
-                LOGGER.warning(
-                    'State not updated. Replication key "%s" not found in record for stream "%s": %s',
-                    stream_defs[tap_stream_id].replication_key,
-                    tap_stream_id,
-                    record,
-                )
-
-                continue
-
-            LOGGER.info("Adding bookmark for %s at %s", tap_stream_id, bookmark_date)
-
-            if not is_substream(stream_defs[tap_stream_id]):
-                state = set_currently_syncing(state, tap_stream_id)
-
-            state = write_bookmark(
-                state,
+        for tap_stream_id, record in stream_def.sync(filter_datetime):  # type: ignore
+            state = handle_record(
                 tap_stream_id,
-                stream_def.replication_key,
-                bookmark_date,
+                record,
+                stream_defs[tap_stream_id],
+                stream_versions[tap_stream_id],
+                state,
             )
-
-            write_state(state)
 
         write_state(state)
 
@@ -200,6 +278,7 @@ def main():
     TAP_CONFIG.api_version = args.config.get("api_version", DEFAULT_API_VERSION)
     TAP_CONFIG.staging = args.config.get("staging", False)
     TAP_CONFIG.api_url = args.config.get("api_url", None)
+    TAP_CONFIG.start_date = args.config["start_date"]
 
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
